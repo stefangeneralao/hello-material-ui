fdc1aa584722695b50ba58e25c56e7f7997e082f
Stefan Generalao <stefan.generalao@gmail.com>
2017-03-13 16:24:21 +0100

    Installed and implemented react-tap-event-plugin.

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import FlatButton from 'material-ui/FlatButton'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 
 export default class App extends Component {
 	render() {
