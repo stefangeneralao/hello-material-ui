dde7a0e04b2b4b39cda408fd702ddcd07e3846d7
Stefan Generalao <stefan.generalao@gmail.com>
2017-03-13 16:19:05 +0100

    Installed and added MuiThemeProvider to App.js. Also added an empty FlatButton.

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import FlatButton from 'material-ui/FlatButton'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          To get started, edit <code>src/App.js</code> and save to reload.
-      </div>
-    );
-  }
+export default class App extends Component {
+	render() {
+		return (
+			<MuiThemeProvider>
+				<div className="App">
+					<FlatButton label="empty button"/>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
-
-export default App;
